827253aa8b7da9d90eda67b5f53f3650dc0701a1
Jessica Reid-Harris <jrharris620@yahoo.com>
2017-10-17 14:01:14 -0500

    Added some instructional commentary

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -19,6 +19,28 @@ namespace LoggingKata
 
         public ITrackable Parse(string line)
         {
+            //Take your line and use string.Split(",", line);
+            //Or it's line.Split(",");
+
+            //If your array.Length is less than 3, something went wrong
+            //Log that and return null
+
+            //grab the long from your array at index 0
+            //grab the lat from your array at index 1
+            //grab the name from your array at index 2
+
+            //You're going to need to parse your string as a Decimal
+            //which is similar to parse a string as an int
+
+            //You'll need to create a TacoBell class
+            //that conforms to ITrackable
+
+            //Then, you'll need an instance of the TacoBell class
+            //With the name and point set correctly
+
+            //Then, return the instance of your TacoBell class
+            //Since it conforms to ITrackable
+
             //DO not fail if one record parsing fails, return null
             return null; //TODO Implement
         }
